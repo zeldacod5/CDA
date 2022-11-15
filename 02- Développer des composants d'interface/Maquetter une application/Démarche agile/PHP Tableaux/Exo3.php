04158e9c0bd2fa376fb4f2e0bd9f190fdfeb5142
zeldacod5 <loucas.herault@laposte.net>
2022-11-15 13:06:53 +0100

    mardi matin

---
+++ 02- Développer des composants d'interface/Maquetter une application/Démarche agile/PHP Tableaux/Exo3.php
@@ -0,0 +1,12 @@
+<?php
+$departements = array(
+    "Hauts-de-france" => array("Aisne", "Nord", "Oise", "Pas-de-Calais", "Somme"),
+    "Bretagne" => array("Côtes d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan"),
+    "Grand-Est" => array("Ardennes", "Aube", "Marne", "Haute-Marne", "Meurthe-et-Moselle", "Meuse", "Moselle", "Bas-Rhin", "Haut-Rhin", "Vosges"),
+    "Normandie" => array("Calvados", "Eure", "Manche", "Orne", "Seine-Maritime")
+);
+
+foreach ($departements as $key => $value){
+    asort($departements);
+    print_r($departements);
+};
